22ad0fb3611224685be9c56c04ff16f6b9167bcd
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-29 12:32:33 -0400

    finished exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import List from "./componenets/List";
 
-function App() {
+const App = ({ store }) => {
+  const { lists, allCards } = store;
+  const createList = list => {
+    const cardList = [];
+    list.cardIds.forEach(id => {
+      if (allCards[id]) {
+        cardList.push(allCards[id]);
+      }
+    });
+    return <List key={list.key} header={list.header} cards={cardList} />;
+  };
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trelloyes!</h1>
       </header>
+
+      <div className="App-list">{lists.map(createList)}</div>
     </div>
   );
-}
+};
 
 export default App;
